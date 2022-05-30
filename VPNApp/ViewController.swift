8316399567609bc00c591c27ebb4f099a9a9ba77
Dzhafar <iamdzhafar@gmail.com>
2022-05-30 22:21:36 +0300

    Initial Commit

---
+++ VPNApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VPNApp
+//
+//  Created by Jafar on 30.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
